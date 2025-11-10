b66c1afe8111e09e76e26197299b92cb2bcbb847
tanulo <tanulo@barossgabor.local>
2025-11-10 11:41:49 +0100

    eddigi órai anyagaok

--- 1_change_event/script.js
+++ 1_change_event/script.js
@@ -2,16 +2,20 @@ const cities = document.querySelector('#cities');
 
 cities.addEventListener('change', () => {
     const selected = cities.value;
-
+    console.log(selected);
+    
+    // a demo azonosítóba beírjuk a kiválasztott város value-t
     const demo = document.querySelector('#demo');
     demo.textContent = selected;
 
-    const image = document.querySelector('#image')
-    const img = document.createElement('img')
-    img.src = `./img/${selected}.jpg`
-    img.alt = selected
-    img.title = selected
-    console.log(img)
+    // beillesztjük a megfelelő képet
+    const image = document.querySelector('#image');
+    const img = document.createElement('img');
+    img.src = `./img/${selected}.jpg`;
+    img.alt = selected;
+    img.title = selected;
+    console.log(img);
 
-    image.replaceChildren(img)
-})
+    image.replaceChildren(img);
+    
+});
--- 2_DOMContentLoad_event/script.js
+++ 2_DOMContentLoad_event/script.js
@@ -1,6 +1,6 @@
 // Városok listája
 const cities = [
-    { name: "Berlin", value: "berlin"},
+    { name: "Berlin", value: "berlin" },
     { name: "Tokyo", value: "tokyo" },
     { name: "Rio", value: "rio" },
     { name: "Nairobi", value: "nairobi" },
@@ -12,21 +12,47 @@ const cities = [
     { name: "Professzor", value: "professor" },
 ];
 
-const select = document.querySelector('#city')
+const select = document.getElementById('city');
 
-window.addEventListener('DOMContentLoaded', betolt);
+window.addEventListener('DOMContentLoaded', betolt); // referencia hivatkozás, nem függvényhívás, azaz nem betolt()
+select.addEventListener('change', kep); // itt is referencia hivatkozás, azaz nem kep()
 
 function betolt() {
-    cities.forEach(city => {
-        const option = document.createElement('option')
-
-        option.value = city.value
-
+    for (const city of cities) {
+        // <option> </option> tag létrehozása
+        const option = document.createElement('option');
+        // <option value="város_neve"> </option> létrehozása
+        option.value = city.value;
+        // <option value="város_neve">város_neve</option> létrehozása
         option.textContent = city.name;
 
-        if (city.name === 'Professor'){
+        if (city.name === 'Professzor') {
             option.selected = true;
         }
-        select.append(option)
-    })
+        // a <select> tag gyerekelemének tesszük meg az option-t
+        // <select>
+        //    <option value="város_neve">város_neve</option>
+        // </select>
+        select.append(option);
+    }
+}
+
+function kep() {
+    const selected = select.value;
+    const image = document.getElementById('image');
+    const demo = document.getElementById('demo');
+
+    // <img> tag létrehozása
+    const img = document.createElement('img');
+    // <img src="./img/${selected}.jpg"> létrehozása
+    img.src = `./img/${selected}.jpg`;
+    // <img src="./img/${selected}.jpg" alt="valami"> létrehozása
+    img.alt = selected;
+    // <img src="./img/${selected}.jpg" alt="valami" title="valami"> létrehozása
+    img.title = selected;
+
+    // az image változóba, ami egy <div id="image"> </div>, belefűzzük az img változót mint gyerekelem, de úgy, hogy előtte a div-ben lévő elemeket kicseréljük az újra (a korábbi elemek eltünnek a div-ből).
+    image.replaceChildren(img);
+    // itt pedig csak a szöveget adjuk hozzá a demo változóhoz, ami egy <span id="demo"></span> tag.
+    demo.textContent = selected;
 }
